b63fd39f238c312c60108b4213f6edfd840c0eb5
Stefan Luger <stefan.luger@hexagon.com>
2024-02-09 15:03:04 +0100

    shaders and first triangle

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 #include <iostream>
 
 #include <GL/glew.h>
@@ -11,34 +12,143 @@
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/string_cast.hpp>
 
-// window dimensions
+// Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
 
-int main(int argc, const char *argv[])
+GLuint VBO, VAO, shader;
+
+// Vertex Shader code
+static const char *vShader = "                                                \n\
+#version 330                                                                  \n\
+                                                                              \n\
+layout (location = 0) in vec3 pos;											  \n\
+                                                                              \n\
+void main()                                                                   \n\
+{                                                                             \n\
+    gl_Position = vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);				  \n\
+}";
+
+// Fragment Shader
+static const char *fShader = "                                                \n\
+#version 330                                                                  \n\
+                                                                              \n\
+out vec4 colour;                                                               \n\
+                                                                              \n\
+void main()                                                                   \n\
+{                                                                             \n\
+    colour = vec4(1.0, 0.0, 0.0, 1.0);                                         \n\
+}";
+
+void CreateTriangle()
+{
+  GLfloat vertices[] = {
+      -1.0f, -1.0f, 0.0f, //
+      1.0f, -1.0f, 0.0f,  //
+      0.0f, 1.0f, 0.0f    //
+  };
+
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  glBindVertexArray(0);
+}
+
+void AddShader(GLuint theProgram, const char *shaderCode, GLenum shaderType)
+{
+  GLuint theShader = glCreateShader(shaderType);
+
+  const GLchar *theCode[1];
+  theCode[0] = shaderCode;
+
+  GLint codeLength[1];
+  codeLength[0] = strlen(shaderCode);
+
+  glShaderSource(theShader, 1, theCode, codeLength);
+  glCompileShader(theShader);
+
+  GLint result = 0;
+  GLchar eLog[1024] = {0};
+
+  glGetShaderiv(theShader, GL_COMPILE_STATUS, &result);
+  if (!result)
+  {
+    glGetShaderInfoLog(theShader, 1024, NULL, eLog);
+    fprintf(stderr, "Error compiling the %d shader: '%s'\n", shaderType, eLog);
+    return;
+  }
+
+  glAttachShader(theProgram, theShader);
+}
+
+void CompileShaders()
+{
+  shader = glCreateProgram();
+
+  if (!shader)
+  {
+    printf("Failed to create shader\n");
+    return;
+  }
+
+  AddShader(shader, vShader, GL_VERTEX_SHADER);
+  AddShader(shader, fShader, GL_FRAGMENT_SHADER);
+
+  GLint result = 0;
+  GLchar eLog[1024] = {0};
+
+  glLinkProgram(shader);
+  glGetProgramiv(shader, GL_LINK_STATUS, &result);
+  if (!result)
+  {
+    glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+    printf("Error linking program: '%s'\n", eLog);
+    return;
+  }
+
+  glValidateProgram(shader);
+  glGetProgramiv(shader, GL_VALIDATE_STATUS, &result);
+  if (!result)
+  {
+    glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+    printf("Error validating program: '%s'\n", eLog);
+    return;
+  }
+}
+
+int main()
 {
   // create a vector with glm and print it to the console
   glm::vec3 v = glm::vec3(-1.0f, 0.0f, 0.0f);
   std::cout << glm::to_string(v) << std::endl;
 
-  //  init GLFW
+  // Initialise GLFW
   if (!glfwInit())
   {
-    printf("GLFW init failed!");
+    printf("GLFW initialisation failed!");
     glfwTerminate();
     return 1;
   }
 
-  // setup GLFW window properties
-  // openGL version
+  // Setup GLFW window properties
+  // OpenGL version
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  // core profile = no backwards compatibility
+  // Core Profile
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  // allow forward compatibility
+  // Allow Forward Compatbility
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
+  // Create the window
   GLFWwindow *mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", NULL, NULL);
-
   if (!mainWindow)
   {
     printf("GLFW window creation failed!");
@@ -46,23 +156,29 @@ int main(int argc, const char *argv[])
     return 1;
   }
 
-  // get buffer size information
+  // Get Buffer Size information
   int bufferWidth, bufferHeight;
   glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
 
-  // set context for GLEW to use
+  // Set context for GLEW to use
   glfwMakeContextCurrent(mainWindow);
 
-  // allow modern extension features
+  // Allow modern extension features
   glewExperimental = GL_TRUE;
 
   if (glewInit() != GLEW_OK)
   {
-    printf("GLEW init failed!");
+    printf("GLEW initialisation failed!");
     glfwDestroyWindow(mainWindow);
     glfwTerminate();
     return 1;
   }
+
+  // Setup Viewport size
+  glViewport(0, 0, bufferWidth, bufferHeight);
+
+  CreateTriangle();
+  CompileShaders();
 
   // setup viewport size
   glViewport(0, 0, bufferWidth, bufferHeight);
@@ -70,12 +186,20 @@ int main(int argc, const char *argv[])
   // loop until window closed
   while (!glfwWindowShouldClose(mainWindow))
   {
-    // get and handle user input events
+    // Get + Handle user input events
     glfwPollEvents();
 
-    // clear window
-    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    // Clear window
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shader);
+
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindVertexArray(0);
+
+    glUseProgram(0);
 
     glfwSwapBuffers(mainWindow);
   }
